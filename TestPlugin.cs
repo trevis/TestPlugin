a82cceb9280791dacd6a43b2b106d79bab02e0f3
trevis <trevis@haxit.org>
2025-02-01 22:41:15 -0700

    initial

---
+++ TestPlugin.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System;
+using Microsoft.Extensions.Logging;
+using Chorizite.Core.Plugins;
+using Chorizite.Core.Plugins.AssemblyLoader;
+using Chorizite.Core;
+
+namespace TestPlugin {
+    /// <summary>
+    /// CoreTestPlugin
+    /// </summary>
+    public class TestPluginCore : IPluginCore {
+        internal static ILogger? Log;
+
+        protected TestPluginCore(AssemblyPluginManifest manifest, ILogger log) : base(manifest) {
+            Log = log;
+
+            Log?.LogDebug($"TestPlugin Version: {Manifest.Version}");
+        }
+
+        protected override void Dispose() {
+            
+        }
+    }
+}
